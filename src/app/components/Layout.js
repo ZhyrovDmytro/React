86a5cae64e433ba4b856fc83a05da7a0332d2501
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-01-26 18:11:14 +0100

    2

--- src/app/components/Layout.js
+++ src/app/components/Layout.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import App from './App';
+import TodosList from './TodosList';
 
 export default class Layout extends Component {
 
     render() {
         return (
             <div>
-                <Header />
+                <TodosList />
+                <App />
             </div>
         );
     }
